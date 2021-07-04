3fb174ca5e609cf18d14f064f34ed0c29b14af42
sdilipkumar0306 <76509014+sdilipkumar0306@users.noreply.github.com>
2021-07-04 18:21:13 +0530

    added alot

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
-$('.tdnn').click(function () {
-  $(".moon").toggleClass('sun');
-  $(".tdnn").toggleClass('day');
+$('.dark-mode').click(function () {
+  $(".dark").toggleClass(' fa-moon fa-sun');
+  $(".border").toggleClass(' border-dark border-light');
+  // $(".dark").removeClass('');
   $(".body").toggleClass("bg-dark");  
+  $(".body").toggleClass("text-white"); 
 });
+
+
+$('#about-tab').click(function () {
+  $(".about").addClass("p0").removeClass("p3 p1 p2 p4 p5");
+  $(".resume").addClass("p2").removeClass("p4 p5 p3 p1 p0");
+  $(".projects").addClass("p2").removeClass("p5 p4 p3 p1 p0");
+
+  $("#about-tab").addClass("active-1");
+  $("#resume-tab").removeClass("active-1");
+  $("#projects-tab").removeClass("active-1");
+
+});
+$('#resume-tab').click(function () {
+  console.log("clicked");
+  $(".about").addClass("p3").removeClass("p0 p1 p2 p4 p5");
+  $(".resume").addClass("p4").removeClass("p0 p1 p2 p3 p5");
+  $(".projects").addClass("p2").removeClass("p5 p0 p1 p3 p4");
+  
+  $("#about-tab").removeClass("active-1");
+  $("#resume-tab").addClass("active-1");
+  $("#projects-tab").removeClass("active-1");
+});
+$('#projects-tab').click(function () {
+  $(".about").addClass("p1").removeClass("p0 p2 p3 p4 p5");
+  $(".resume").addClass("p2").removeClass("p4 p3  p1 p0 p5");
+  $(".projects").addClass("p5").removeClass("p2 p0 p1 p3 p4");
+
+  $("#about-tab").removeClass("active-1");
+  $("#resume-tab").removeClass("active-1");
+  $("#projects-tab").addClass("active-1");
+});
+
 
